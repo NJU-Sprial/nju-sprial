d251d2d5744fafcb4fc316ffb707756b15ac16d0
151250212 <151250212@smail.nju.edu.cn>
2017-09-02 15:12:53 +0800

    添加用户类型

---
+++ src/main/java/vo/UserType.java
@@ -0,0 +1,27 @@
+package vo;
+
+/**
+ * Created by zjy on 2017/9/2.
+ * 用户类型
+ */
+public enum UserType {
+    /**
+     * 券商
+     */
+    SecuritiesTrader,
+
+    /**
+     * 投资人
+     */
+    Investor,
+
+    /**
+     * 法律事务所
+     */
+    LawFirm,
+
+    /**
+     * 律师事务所
+     */
+    LawyerFirm,
+}
