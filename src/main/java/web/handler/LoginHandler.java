99f39562da729b9e6a941e83f97515630d165a92
151250212 <151250212@smail.nju.edu.cn>
2017-09-02 23:38:54 +0800

    去掉路由中的.do后缀

--- src/main/java/web/handler/LoginHandler.java
+++ src/main/java/web/handler/LoginHandler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginHandler {
-	@RequestMapping("/login.do")
+	@RequestMapping("/login")
 	public String login(String account,String password) {
 		System.out.println("我进来了");
 		return "login";
