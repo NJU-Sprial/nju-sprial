fddfd04458a746237f6279e1c60c978d7f46f3fb
WaterWan <598127577@qq.com>
2017-09-03 21:09:36 +0800

    删掉了一个错误引用的包

--- src/main/java/po/ProductPO.java
+++ src/main/java/po/ProductPO.java
@@ -3,7 +3,6 @@ package po;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.time.LocalDateTimeTime;
 
 @Entity
 @Table(name = "product", schema = "sprial")
