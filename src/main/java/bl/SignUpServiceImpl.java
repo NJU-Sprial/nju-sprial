9e78c635686c89b1cf35f27b7cc80152765beb04
WaterWan <598127577@qq.com>
2017-09-04 17:23:24 +0800

    修改登录的实现

--- src/main/java/bl/SignUpServiceImpl.java
+++ src/main/java/bl/SignUpServiceImpl.java
@@ -25,26 +25,27 @@ public class SignUpServiceImpl implements SignUpService{
      * 注意：如果注册成功，账号还不能使用。在注册成功后三个工作日内工作人员会打电话给用户，核实信息，如果通过，
      * 会发一封邮件给用户，然后账号才能使用
      * </p>
-     * TODO:改过接口
      * @param signUpVO 用户填写的注册信息
      * @return 成功返回true，失败返回false
      * @throws SignUpException 注册失败时提示原因
      */
     @Override
     public boolean signUp(SignUpVO  signUpVO) throws SignUpException {
-       /* if(!signUpVO.getPassword().equals(signUpVO.getPassword_confirm())) {
+       if(!signUpVO.getPassword().equals(signUpVO.getPassword_confirm())) {
             throw new SignUpException(SignUpCode.PASSWORD_NOT_SAME);
         }
-        SignUpPO signUpPO = new SignUpPO();
-        String ignoreProperty = "password_confirm";
-        BeanUtils.copyProperties(signUpVO, signUpPO, ignoreProperty);
-        String result = userDataService.signUp(signUpPO);
+       UserDataPO userDataPO = new UserDataPO();
+        String ignoreProperty1 = "serialVersionUID";
+        String ignoreProperty2 = "password_confirm";
+        String ignoreProperty3 = "userState";
+        String ignoreProperty4 = "offline";
+        BeanUtils.copyProperties(signUpVO, userDataPO, ignoreProperty1, ignoreProperty2, ignoreProperty3, ignoreProperty4);
+        String result = userDataService.signUp(userDataPO);
         if(result.equals(SignUpCode.SIGN_UP_SUCCESS)) {
             return true;
         } else {
             throw new SignUpException(result);
-        }*/
-       return  false;
+        }
     }
 
     /**
