3f3a8a31daeb99f943338b437a7e33dad0a08762
KingOfSolo <944125893@qq.com>
2017-09-06 20:48:12 +0800

    资产包统计分析页面

--- src/main/webapp/js/assetpackage.js
+++ src/main/webapp/js/assetpackage.js
@@ -8,12 +8,14 @@ $(function () {
             $(this).attr("placeholder","搜索");
             $(this).animate({width:"120px"});
             $(this).css("border-color","#999");
+            $(this).css("box-shadow","0px 0px 0px #18ba60");
             $(".search-button").removeClass("green");
         }
     }).focus(function () {
         $(this).animate({width:"280px"});
         $(this).attr("placeholder","请输入资产包编号");
         $(this).css("border-color","#18ba60");
+        $(this).css("box-shadow","0px 0px 2px #18ba60");
         $(".search-button").addClass("green");
     })
 })
