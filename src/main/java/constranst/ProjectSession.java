b9f291845b4a93d5120fe1ab40ef343df106a51e
xiekailian <460715662@qq.com>
2017-09-12 22:07:39 +0800

    解决数据传输表单属性和文件无法同时传输的天坑

---
+++ src/main/java/constranst/ProjectSession.java
@@ -0,0 +1,13 @@
+package constranst;
+
+/**
+ * Created by Water on 2017/9/3.
+ */
+public class ProjectSession {
+    public static final String PNAME = "pname";
+
+    public static final String PTYPE = "ptype";
+
+    public static final String PWAY = "pway";
+
+}
