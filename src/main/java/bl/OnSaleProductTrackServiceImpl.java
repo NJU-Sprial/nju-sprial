ccfa026a6bfcbeb39d7061f1c86f55f5d0d08548
WaterWan <598127577@qq.com>
2017-10-03 11:21:23 +0800

    实现了第一个方法

--- src/main/java/bl/OnSaleProductTrackServiceImpl.java
+++ src/main/java/bl/OnSaleProductTrackServiceImpl.java
@@ -1,6 +1,10 @@
 package bl;
 
 import blservice.OnSaleProductTrackService;
+import dataservice.AssetPoolChangeLogDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import po.AssetPoolChangeLogPO;
+import util.PO2VOUtil;
 import vo.AssetPoolChangeLogVO;
 import vo.SecuritiesMessageVO;
 import vo.SecuritiesRepaymentVO;
@@ -11,15 +15,22 @@ import java.util.List;
  * Created by Water on 2017/10/3.
  */
 public class OnSaleProductTrackServiceImpl implements OnSaleProductTrackService {
+
+    private AssetPoolChangeLogDataService assetPoolChangeLogDataService;
+    @Autowired
+    public OnSaleProductTrackServiceImpl(AssetPoolChangeLogDataService assetPoolChangeLogDataService) {
+        this.assetPoolChangeLogDataService = assetPoolChangeLogDataService;
+    }
     /**
      * 获取资产池变动情况
-     * TODO wz
+     * @see po.AssetPoolChangeLogPO
      * @param productID 产品ID
      * @return 如果产品不存在，返回null
      */
     @Override
     public List<AssetPoolChangeLogVO> getAssetPoolChangeLog(String productID) {
-        return null;
+        List<AssetPoolChangeLogPO> assetPoolChangeLogPOs = assetPoolChangeLogDataService.getAssetPoolChangeLog(productID);
+        return PO2VOUtil.assetPoolChangeLogPOs2VOs(assetPoolChangeLogPOs);
     }
 
     /**
--- src/main/java/util/PO2VOUtil.java
+++ src/main/java/util/PO2VOUtil.java
@@ -1,7 +1,9 @@
 package util;
 
 import org.springframework.beans.BeanUtils;
+import po.AssetPoolChangeLogPO;
 import po.ProductPO;
+import vo.AssetPoolChangeLogVO;
 import vo.ProductVO;
 
 import java.util.ArrayList;
@@ -23,9 +25,39 @@ public class PO2VOUtil {
     }
 
     public static List<ProductVO> productPOs2VOs(List<ProductPO> productPOs) {
+        if(productPOs == null) {
+            return null;
+        }
         List<ProductVO> result = new ArrayList<>();
         for(ProductPO po : productPOs) {
             result.add(productPO2VO(po));
+        }
+        return result;
+    }
+
+    private static AssetPoolChangeLogVO assetPoolChangeLogPO2VO(AssetPoolChangeLogPO assetPoolChangeLogPO) {
+        AssetPoolChangeLogVO assetPoolChangeLogVO = new AssetPoolChangeLogVO();
+        assetPoolChangeLogVO.setAnnouncementDate(assetPoolChangeLogPO.getAnnouncementDate().toLocalDateTime().toLocalDate());
+        assetPoolChangeLogVO.setReceiptStartDate(assetPoolChangeLogPO.getReceiptStartDate().toLocalDateTime().toLocalDate());
+        assetPoolChangeLogVO.setReceiptEndDate(assetPoolChangeLogPO.getReceiptEndDate().toLocalDateTime().toLocalDate());
+        assetPoolChangeLogVO.setInitialPrincipalBalance(assetPoolChangeLogPO.getInitialPrincipalBalance());
+        assetPoolChangeLogVO.setFinalPrincipalBalance(assetPoolChangeLogPO.getFinalPrincipalBalance());
+        assetPoolChangeLogVO.setPrincipalRecoveryBalance(assetPoolChangeLogPO.getPrincipalRecoveryBalance());
+        assetPoolChangeLogVO.setLoanNum(assetPoolChangeLogPO.getLoanNum());
+        assetPoolChangeLogVO.setInterestRecoveryBalance(assetPoolChangeLogPO.getInterestRecoveryBalance());
+        assetPoolChangeLogVO.setPrincipalRecoveryRate(assetPoolChangeLogPO.getPrincipalRecoveryRate());
+        assetPoolChangeLogVO.setAveLoanRate(assetPoolChangeLogPO.getAveLoanRate());
+        assetPoolChangeLogVO.setAveLeftMonth(assetPoolChangeLogPO.getAveLeftMonth());
+        return assetPoolChangeLogVO;
+    }
+
+    public static List<AssetPoolChangeLogVO> assetPoolChangeLogPOs2VOs(List<AssetPoolChangeLogPO> assetPoolChangeLogPOs) {
+        if(assetPoolChangeLogPOs == null) {
+            return null;
+        }
+        List<AssetPoolChangeLogVO> result = new ArrayList<>();
+        for(AssetPoolChangeLogPO po : assetPoolChangeLogPOs) {
+            result.add(assetPoolChangeLogPO2VO(po));
         }
         return result;
     }
