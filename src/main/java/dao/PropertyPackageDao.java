30d0233e2b313d4ac65b80d788b35d6eb849fd0a
WaterWan <598127577@qq.com>
2017-11-19 15:01:29 +0800

    删掉一个没有用的PO以及对应的数据层和持久层代码

--- src/main/java/dao/PropertyPackageDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import po.PropertyPackagePO;
-
-/**
- * Created by yinywf on 2017/9/7
- */
-public interface PropertyPackageDao extends SimpleHibernateDao<PropertyPackagePO,Integer> {
-}
